4f61b187816e2d88ade812eb30765b6a8b68189e
WeddingCrashers <hangu4043@gmail.com>
2024-11-08 02:04:47 +0900

    [241108]iTransformer 구현

--- models/iTransformer.py
+++ models/iTransformer.py
@@ -2,23 +2,16 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 from layers.Embed import DataEmbedding_inverted
+from layers.Transformer_EncDec import Encoder, EncoderLayer
+from layers.Attentions import FullAttention, AttentionLayer
 import numpy as np
 
 
 class Model(nn.Module):
     
-    
     # torch 모든 Layer들은 nn.Module 상속
     def __init__(self, configs):
-        super(Model, self).__init__()ncoderLayer(
-                            AttentionLayer(
-                                FullAttention(False, configs.factor, attention_dropout=configs.dropout,
-                                              output_attention=configs.output_attention), configs.d_model, configs.n_heads),
-                            configs.d_model,
-                            configs.d_ff,
-                            dropout=configs.dropout,
-                            activation=configs.activation
-                        ) for l in range(configs.e_layers)
+        super(Model, self).__init__()
         # Module의 생성자 상속으로 실행
         self.seq_len = configs.seq_len
         self.pred_len = configs.pred_len
@@ -36,4 +29,34 @@ class Model(nn.Module):
         self.class_strategy = configs.class_strategy
         
         #Encoder-only architecture 
-        # self.encoder 
+        self.encoder = Encoder(
+            [
+                EncoderLayer(
+                    AttentionLayer(
+                        FullAttention(False, configs.factor, attention_dropout=configs.dropout, output_attention=configs.ouput_attention), configs.d_model, configs.n_heads),configs.d_model, configs.d_ff, dropout = configs.dropout, activation = configs.activation
+                    ) for l in range(configs.e_layers)
+            ],
+            norm_layer = torch.nn.LayerNorm(configs.d_model)    
+            
+        ) 
+        self.projector = nn.Linear(configs.d_model, configs.pred_len, bias = True)
+        
+        def forecast(self, x_enc,x_mark_enc,x_dec, x_mark_dec):
+            if self.use_norm:
+                means = x_enc.mean(1, keepdim=True).detach()
+                x_enc = x_enc - means
+                stdev = torch.sqrt(torch.var(x_enc, dim=1, keepdim=True, unbiased=False) + 1e-5)
+                x_enc /= stdev
+                
+            _,_,N = x_enc.shape 
+            
+            enc_out = self.enc_embedding(x_enc,x_mark_enc)
+            enc_out, attns = self.encoder(enc_out, attn_mask=None)
+            dec_out  = self.projector(enc_out).permute(0,2,1)[:,:,:N] #filter covariates
+            
+            if self.use_norm:
+                # De-Normalization from Non-stationary Transformer
+                dec_out = dec_out * (stdev[:, 0, :].unsqueeze(1).repeat(1, self.pred_len, 1))
+                dec_out = dec_out + (means[:, 0, :].unsqueeze(1).repeat(1, self.pred_len, 1))
+
+            return dec_out
